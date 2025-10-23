b0c470d56609708c52a5dbcb7319d4d6bf23f5bf
Fez <FernandoLpz0911.business@Gmail.com>
2025-10-22 20:22:52 -0500

    created some django routing

--- backend/backendlogic/api/views.py
+++ backend/backendlogic/api/views.py
@@ -1,3 +1,11 @@
+# Contains the functions for http requests then spits out a response
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def api(request):
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
 
 # Create your views here.
