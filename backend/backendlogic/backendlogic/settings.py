413e97093e8b46ef27d78066ef54b7e66c78aad2
FernandoLpz0911 <FernandoLpz0911.business@Gmail.com>
2025-12-01 23:21:51 -0600

    I think this slash literally cause compilation failure bruh

--- backend/backendlogic/backendlogic/settings.py
+++ backend/backendlogic/backendlogic/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",             
     "http://127.0.0.1:5173",             
-    "https://ai-watermark-registry.vercel.app/"
+    "https://ai-watermark-registry.vercel.app"
 ]
